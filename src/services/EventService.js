a97a8e99b36a3d48c582cf35d984816d4e4d29cc
overbyclock <overbyclock@gmail.com>
2022-07-28 11:33:00 +0200

    lesson-4

---
+++ src/services/EventService.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const Apiclient = axios.create({
+  baseURL: "https://my-json-server.typicode.com/Code-Pop/Real-World_Vue-3",
+  withCredentials: false,
+  headers: {
+    Accept: "aplication/json",
+    "Content-Type": "aplication/json",
+  },
+});
+
+export default {
+  getEvents() {
+    return Apiclient.get("/events");
+  },
+};
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Our goal is to change the world doing small things</h1>
   </div>
 </template>
